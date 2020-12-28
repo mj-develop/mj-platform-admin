94f0aacab78a923bd7bfa17fa30417e99979e9f5
thiagoos16 <tos@icomp.ufam.edu.br>
2020-12-28 16:02:06 -0400

    add initial configuration adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Aprender portal dos módulos.
         </p>
         <a
           className="App-link"
