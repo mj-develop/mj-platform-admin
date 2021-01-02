dec2526c0bb833b9a61a679bd7bbd9f247283b9a
thiagoos16 <tos@icomp.ufam.edu.br>
2021-01-02 18:13:20 -0400

    add adjusts in sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from 'react-icons/md';
+
+import logoImg from '../../assets/logo.svg'
+
+import { Container, Header, LogoImg, MenuContainer, MenuItemLink, Title } from './styles';
 
 const Sidebar: React.FC = () => {
     return (
         <Container>
-            <h1> Sidebar </h1>
+           <Header>
+               <LogoImg src={logoImg} alt="Logo Aprender"/>
+               <Title> Aprender Portal </Title>
+           </Header>
+
+           <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard/>
+                    Dashboard
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowUpward/>
+                    Professores
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowDownward/>
+                    Alunos
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdExitToApp/>
+                    Sair                
+                </MenuItemLink>
+           </MenuContainer>
         </Container>
     );
 }
