7dcfc3d321f34eda35f4f484e11e6fb90aa6570e
thiagoos16 <tos@icomp.ufam.edu.br>
2021-01-03 14:51:44 -0400

    add adjust to selectinput set in contentheader

--- src/components/ContentHeader/styles.ts
+++ src/components/ContentHeader/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+interface ITitleContainerProps {
+    lineColor: string;
+} 
+
 export const Container = styled.div `
     width: 100%;
     display: flex;
@@ -10,14 +14,15 @@ export const Container = styled.div `
     }
 `;
 
-export const TitleContainer = styled.div `
+export const TitleContainer = styled.div<ITitleContainerProps> `
     &::after {
         content: '';
         display: block;
         width: 55px;
-        border-bottom: 10px solid ${props => props.theme.colors.warning}
+        border-bottom: 10px solid ${props => props.lineColor}
     }
 `; 
 
 export const Controllers = styled.div`
+    display: flex;
 `;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import SelectInput from '../../components/SelectInput';
 import ContentHeader from '../../components/ContentHeader';
 
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
+    const options = [
+        {value: 'test', label: 'test'}
+    ];
+
     return (
         <Container>
-            <ContentHeader/>
+            <ContentHeader title="Dashboard" lineColor="#FFF"> 
+                <SelectInput options={options}/>
+            </ContentHeader>
         </Container>
     );
 }
